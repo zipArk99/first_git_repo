21e9ba6a1f67f9ee3e529f17781df69cec7550a1
Shaurya Kajiwala <kajiwalashaurya29@gmail.com>
2020-08-02 10:46:35 -0400

    sec git understood

---
+++ sec_git.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int lit=10;
+
+    for(int i=1; i<=10; i+=1)
+    {
+             int n=5;
+             int multi=n*i;
+
+             cout<<n<<"x"<<i<<"::"<<multi<<endl;
+
+    }
+    return 0;
+
+}
